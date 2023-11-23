9f308768051ebe554a08e31e4baf34bdeba11df6
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2023-11-23 23:42:17 +0200

    Null safety

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,3 @@ import 'package:flutter/material.dart';
 
 import 'every_widgets/about_dialog.dart';
 
-const Color darkBlue = Color(0xFF12202F);
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
-      debugShowCheckedModeBanner: false,
-      title: 'Every Flutter Widget',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Mapp'),
-        ),
-        //! Just change the Widget001 for the Widget you want.
-        body: const Widget001(),
-      ),
-    );
-  }
-}
